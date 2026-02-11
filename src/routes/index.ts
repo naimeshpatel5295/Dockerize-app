9d7d31200292b12ac34334fff692f87f1defdd67
Naimesh Patel <naimeshpatel5295@gmail.com>
2026-02-11 17:13:39 +0530

    feat: add version routes for blue-green demo

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import healthRoutes from "./health.routes";
 import noteRoutes from "./note.routes";
+import versionRoutes from "./version.routes";
+
 
 const router = Router();
 
+// Health routes
 router.use("/health", healthRoutes);
+// Notes routes
 router.use("/notes", noteRoutes);
+// Blue-Green demo endpoint
+router.use("/version", versionRoutes);
+
 
 export default router;
